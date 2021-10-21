946d5389fe3fd74b4c364c46f68d3f26df449e18
dannygonzalezchisica <dannychisica@gmail.com>
2021-10-20 20:51:19 -0500

    Revert "Revert "corregir url de logout""
    This reverts commit 09a24d458c4b3b6261842c546145957db123c3fe.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ const Sidebar = () => {
     const { user, logout } = useAuth0();
 
     const cerrarSesion = () => {
-        logout({ returnTo: 'http://localhost:3000/admin' })
+        logout({ returnTo: 'https://radiant-escarpment-02424.herokuapp.com/admin' })
         localStorage.setItem('token', null)
     }
 
